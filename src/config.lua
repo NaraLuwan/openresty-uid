e00e36fb7c395f531e1b8eeb41779a80a1feffd1
何超 <hechao@corp.netease.com>
2020-09-18 13:58:49 +0800

    init

---
+++ src/config.lua
@@ -0,0 +1,11 @@
+local config = {};
+config['mysql_host'] = "127.0.0.1"
+config['mysql_port'] = "3306"
+config['mysql_user'] = "root"
+config['mysql_pass'] = ""
+config['mysql_db'] = "agw"
+
+config["redis_host"] = "127.0.0.1"
+config["redis_port"] = 6379
+
+return config;
